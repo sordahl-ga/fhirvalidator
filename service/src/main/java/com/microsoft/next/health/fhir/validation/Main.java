b6a96b24867acebb2f1371e5cec737dafea4c680
Steve <sordahl@bellsouth.net>
2022-08-03 15:45:49 -0400

    init

---
+++ service/src/main/java/com/microsoft/next/health/fhir/validation/Main.java
@@ -0,0 +1,48 @@
+package com.microsoft.next.health.fhir.validation;
+
+import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+        //System.out.println("Initializing FHIRValidation Engine");
+        //ValidationHolder.getEngineInstance();
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+        tomcat.getConnector(); // Trigger the creation of the default connector
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        StandardJarScanner scanner=new StandardJarScanner();
+        scanner.setScanClassPath( false );
+        scanner.setScanManifest( false );
+        ctx.setJarScanner(scanner);
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
